e091acd78c56f2d1737d05a200048ecbe73ee9df
Ashish <ashish78337@gmail.com>
2026-01-19 20:51:34 +0530

    Basic Backtracking

--- Recursion/Basic.cpp
+++ Recursion/Basic.cpp
@@ -7,6 +7,15 @@ void fun(int i,int n){
     fun(i+1,n);
 }
 
+//Backtracking method
+void funbt(int i,int n){
+    if(i<1) return ;
+    funbt(i-1,n);
+    cout << i << endl;
+}
+
+
 int main(){
-    fun(1,4);
+    // fun(1,4);
+    funbt(4,4);
 }
